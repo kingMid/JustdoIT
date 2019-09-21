29b7bf5daa6c420cdc1fbe92404abba41312c0a7
kingMid <987322723@qq.com>
2019-09-21 10:53:44 +0800

    921

---
+++ KnowledgePoints/VueCli/vuecli3-test/src/views/Mine.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+this is my html
+    <router-link to="">路由1</router-link>
+    <router-link to="">路由2</router-link>
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
